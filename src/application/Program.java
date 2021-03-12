6144e127a66bd9ad8f2ccea8ab9a9520c04717df
DionatanLeao <dionatan.ti@outlook.com>
2021-03-12 16:01:18 -0300

    Começando a implementar Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 /***
  * 
@@ -11,9 +11,8 @@ import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Position pos = new Position(3, 5);	
-		System.out.println(pos);
+
+		Board board = new Board(8, 8);
 
 	}
 
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,23 @@
+package boardgame;
+
+/***
+ * 
+ * @author dionatan
+ *
+ */
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+}
